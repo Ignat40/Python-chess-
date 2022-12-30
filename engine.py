5608f70a02cd0c2f69d8e9b60671c836ab460b0d
ignat40 <ignatbojinov@gmail.com>
2022-12-30 16:42:12 +0200

    finish king moves

--- engine.py
+++ engine.py
@@ -133,8 +133,15 @@ class chess_board():
         self.get_rook_moves(row, cols, moves)
 
     def get_king_moves(self, row, cols, moves):
-        pass
-    
+        king_moves = ((-1, -1), (-1, -0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        op_color = "w" if self.white_to_move else "b"
+        for i in range(8):
+            end_row = row + king_moves[i][0]
+            end_col = cols + king_moves[i][1]
+            if 0 <= end_row < 8 and 0 <= end_col < 8:
+                end_piece = self.board[end_row][end_col]
+                if end_piece[0] != op_color:
+                    moves.append(Move((row, cols), (end_row, end_col), self.board))    
 
 class Move():
 
